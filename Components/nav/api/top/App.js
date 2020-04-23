98c2e4320136efc7f976364dd731864da0402c00
4NdY-69 <adani.69@icloud.com>
2020-04-23 16:50:57 +0400

    1 file modified

--- Components/nav/api/top/App.js
+++ Components/nav/api/top/App.js
@@ -6,6 +6,7 @@ export default function Top(){
     <nav id="TopNav">
     <button><i class="fas fa-sign-in-alt"></i>&nbsp; تسجيل الدخول </button>
     <span><i class="fas fa-bell"></i>&nbsp; التنبيهات</span>
+    <span><img src="https://i.ibb.co/qkXKLcR/53e0d3434a55aa14f6da8c7dda79367e103adae05b526c4870-1.png" /></span>
     </nav>
   )
 }
